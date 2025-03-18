ec6e377cfcada5fbf1e844880ae89525d7f7302f
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-18 23:09:51 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let body = document.body;
+let main = document.querySelector("main");
+main.remove();
+let newHeader = document.createElement("h1");
+body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "VICTOR is the champion";
